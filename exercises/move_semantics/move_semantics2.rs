0a58fbaa3217c5d09e6c143cfa52f431c9b79b75
Nathan Wallace <nwallace@salesforce.com>
2021-04-20 10:38:34 -0400

    Saving progress

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -2,12 +2,12 @@
 // Make me compile without changing line 13!
 // Execute `rustlings hint move_semantics2` for hints :)
 
-// I AM NOT DONE
+// NBW: So Vecs aren't Copy. Instead, we moved vec0 into fill_vec and can't use it afterward
 
 fn main() {
     let vec0 = Vec::new();
 
-    let mut vec1 = fill_vec(vec0);
+    let mut vec1 = fill_vec(vec0.clone());
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
